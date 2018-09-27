5ad3a9998b30961aeb541a8f49c714ed15c99335
Martin Guardado <martinguardado@gmail.com>
2018-09-26 23:06:28 -0600

    Remove phone formatting, test commit

--- src/components/Client/List/TableBody.js
+++ src/components/Client/List/TableBody.js
@@ -1,12 +1,6 @@
 import React from "react";
 
 class TableBody extends React.Component {
-   // todo: function not working, is this an issue with React or Redux?
-   // formatPhone = phone => {
-   //    let copy = phone;
-   //    return copy.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
-   // };
-
    render() {
       const tableRows = this.props.clients.map(client => {
          const {
