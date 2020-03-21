8e63a41876de748df51d62a8a19d6f3434ce366f
Martin Guardado <martinguardado@gmail.com>
2020-03-21 15:56:55 -0600

    update to use env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 
+const API_SERVER_URL = process.env.REACT_APP_API_URL;
+
 export function setTokenHeader(token) {
-   if (token) {
-      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-   } else {
-      delete axios.defaults.headers.common["Authorization"];
-   }
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
 }
 
 export function apiCall(method, path, data) {
-   return new Promise((resolve, reject) => {
-      return axios[method](path, data)
-         .then(res => {
-            return resolve(res.data);
-         })
-         .catch(err => {
-            return reject(err.response.data.error);
-         });
-   });
+  const url = API_SERVER_URL + path;
+  return new Promise((resolve, reject) => {
+    return axios[method](url, data)
+      .then(res => {
+        return resolve(res.data);
+      })
+      .catch(err => {
+        return reject(err.response.data.error);
+      });
+  });
 }
